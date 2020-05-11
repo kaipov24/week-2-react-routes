8a11f69e239af8416c8fdfd49f07e9781eb55485
Kayrat <kaipov.kayrat@gmail.com>
2020-05-12 02:56:01 +0600

    first commit

--- client/components/home.js
+++ client/components/home.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
 import Header from './header'
+import Profile from './profile'
+import Main from './main'
+
 
 const Home = () => {
   return (
@@ -7,7 +11,14 @@ const Home = () => {
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          {/* add your routes here */}
+          <Switch>
+            <Route exact path="/dashboard/" component={() => <Home />} />
+            <Route exact path="/dashboard/main" component={() => <Main />} />
+            <Route exact path="/dashboard/profile/:user" component={() => <Profile />} />
+          </Switch>
+          <div><Link to="/dashboard">Go To Root</Link></div>
+          <div><Link to="/dashboard/main">Go To Main</Link></div>
+          <div><Link to="/dashboard/profile/0355a3e9-4149-4e5d-b3a0-46e25687b070">Go To Profile</Link></div>          
         </div>
       </div>
     </div>
