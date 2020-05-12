d86a7c7d41702e9d4aa09dcdc769a1968dc27f6e
Kayrat Kaipov <59024495+kaipov24@users.noreply.github.com>
2020-05-12 17:48:59 +0600

    home

--- client/components/home.js
+++ client/components/home.js
@@ -5,7 +5,6 @@ import Profile from './profile'
 import Main from './main'
 import Dashboard from './dashboard'
 
-
 const Home = () => {
   return (
     <div>
@@ -13,10 +12,10 @@ const Home = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <Switch>
-            <Route exact path="/dashboard" component={() => <Dashboard />} />
+            <Route exact path="/dashboard/" component={() => <Dashboard />} />
             <Route exact path="/dashboard/main" component={() => <Main />} />
             <Route exact path="/dashboard/profile/:user" component={() => <Profile />} />
-          </Switch>       
+          </Switch>
         </div>
       </div>
     </div>
