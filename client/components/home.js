5a636751bcd320eead73660a557b81cd1f090ae3
Kayrat Kaipov <59024495+kaipov24@users.noreply.github.com>
2020-05-12 16:58:38 +0600

    Dashboard updated

--- client/components/home.js
+++ client/components/home.js
@@ -3,6 +3,7 @@ import { Switch, Route, Link } from 'react-router-dom'
 import Header from './header'
 import Profile from './profile'
 import Main from './main'
+import Dashboard from './dashboard'
 
 
 const Home = () => {
@@ -12,7 +13,7 @@ const Home = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <Switch>
-            <Route exact path="/dashboard/" component={() => <Home />} />
+            <Route exact path="/dashboard/" component={() => <Dashboard />} />
             <Route exact path="/dashboard/main" component={() => <Main />} />
             <Route exact path="/dashboard/profile/:user" component={() => <Profile />} />
           </Switch>
