a2b7e6c5bdb6172024f28c3a70db6632749d1446
Kayrat Kaipov <59024495+kaipov24@users.noreply.github.com>
2020-05-12 18:43:17 +0600

    dashboard update2

--- client/components/main.js
+++ client/components/main.js
@@ -6,7 +6,7 @@ const Main = () => {
     <div>
       <div с id="title">Main</div> 
       <Link to="/dashboard/profile/0355a3e9-4149-4e5d-b3a0-46e25687b070">Go To Profile</Link>
-      <Link to="/dashboard/">Go To Root</Link>
+      <Link to="/dashboard">Go To Root</Link>
     </div>
   )
 }
