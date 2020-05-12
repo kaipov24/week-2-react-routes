0683be48f0496be9a35cecd218a4383ea9bf469e
Kayrat Kaipov <59024495+kaipov24@users.noreply.github.com>
2020-05-12 17:55:01 +0600

    dashboard update

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -5,14 +5,9 @@ import { Link } from 'react-router-dom'
 const Dashboard = () => {
   return (
     <div>
-      <Head title="Hello" />
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <div с id="title">Dashboard</div> 
-          <Link to="/dashboard/profile/0355a3e9-4149-4e5d-b3a0-46e25687b070">Go To Profile</Link>
-          <Link to="/dashboard/main">Go To Main</Link>
-        </div>
-      </div>
+      <div с id="title">Dashboard</div> 
+      <Link to="/dashboard/profile/0355a3e9-4149-4e5d-b3a0-46e25687b070">Go To Profile</Link>
+      <Link to="/dashboard/main">Go To Main</Link>
     </div>
   )
 }
